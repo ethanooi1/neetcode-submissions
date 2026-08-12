5bf3807db58c4bf95b0bd2ef3ad98f44b6f33f48
Ethan Ooi <ethan.ooiow@gmail.com>
2026-08-12 18:50:23 -0400

    Add: is-palindrome - submission-20

---
+++ Data Structures & Algorithms/is-palindrome/submission-20.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        L = 0
+        R = len(s)-1
+
+        while L < R:
+            while not s[L].isalnum() and L < R:
+                L += 1
+            while not s[R].isalnum() and L < R:
+                R -= 1
+            if s[L].lower() != s[R].lower():
+                return False
+            L += 1
+            R -= 1
+        
+        return True
