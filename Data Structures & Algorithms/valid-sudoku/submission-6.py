c5ce4b4c821d94eac534e32b6157e22f3419e213
Ethan Ooi <ethan.ooiow@gmail.com>
2026-08-16 16:37:30 -0400

    Add: valid-sudoku - submission-6

---
+++ Data Structures & Algorithms/valid-sudoku/submission-6.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        row = defaultdict(set)
+        col = defaultdict(set)
+        squares = defaultdict(set)
+
+        for r in range(9):
+            for c in range(9):
+                item = board[r][c]
+                if item not in row[r] and item not in col[c] and item not in squares[r//3, c//3]:
+                    row[r].add(item)
+                    col[c].add(item)
+                    squares[r//3, c//3].add(item)
+                elif item == '.':
+                    continue
+                else:
+                    return False
+        
+        return True
